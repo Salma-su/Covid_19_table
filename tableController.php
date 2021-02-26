dc0f264c2d1e88921f1fb8e63128429a2d33e919
Salma-su <79605350+Salma-su@users.noreply.github.com>
2021-02-26 16:48:15 +0100

    Add files via upload

---
+++ tableController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class tableController extends Controller
+{
+    public function func() {
+        
+        $curl = curl_init();
+
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/countries",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => [
+                "x-rapidapi-host: vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com",
+                "x-rapidapi-key: 4c9a058b7fmsha4c2cdba708deaep174172jsn8b3bc1b035a7"
+            ],
+        ]);
+        
+        
+                    $data = curl_exec($curl);
+                    $json = json_decode($data);
+                     
+                    curl_close($curl);
+
+                     
+        return $json ;
+       
+        }
+
+        public function main() {
+        $json = $this->func();
+
+     return view('table',  ['json' => $json]);
+    
+
+    
+    }
+}
